ece71aba16ec99a8a04cba5c2ccc8e0d434055da
Honeyk29 <114303993+Honeyk29@users.noreply.github.com>
2024-03-21 21:16:22 +0530

    Loop on typed.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// setInterval(() => {
+//             var typed = new Typed('#element', {
+//             strings: ['Web Developer', 'Data Analyst','Data Scientist'],
+//             typeSpeed: 50,
+//         });
+            
+//         }, 7000);
