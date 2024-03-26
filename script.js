e4f7d2851836b2153e1097e01474107d654c7b60
Honey kumar <honeyk.parashar@gmail.com>
2024-03-26 11:40:46 +0530

    changed background color and added form

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// setInterval(() => {
-//             var typed = new Typed('#element', {
-//             strings: ['Web Developer', 'Data Analyst','Data Scientist'],
-//             typeSpeed: 50,
-//         });
-            
-//         }, 7000);
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Data Analyst','Data Scientist'],
+    typeSpeed: 50,
+    loop:true,
+});
+document.title = "Honey's portfolio";
+document.getElementById('downloadbutton').addEventListener('click',()=>{
+    var pathfile = 'resume.pdf';
+    var link = document.createElement('a');
+    link.setAttribute('href',pathfile);
+    link.setAttribute('download','');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
